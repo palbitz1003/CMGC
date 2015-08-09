f0799c4180526c4befedb9b78054122661736ab3
palbitz1003 <palbitz@san.rr.com>
2015-08-09 16:00:10 -0700

    Update life member string

--- Web/dues_life_members.php
+++ Web/dues_life_members.php
@@ -13,7 +13,7 @@ echo '    <div id="content" role="main">' . PHP_EOL;
 
 echo '<h2 class="entry-title" style="text-align:center">Dues For Life Members</h2>' . PHP_EOL;
 
-echo '<p>If you are currently a lifetime member please submit a check for $30.00 to cover the cost of maintaining your SCGA handicap. If you will be eligible for a lifetime membership in the coming year, submit your proof of age along with a check for $30.00 if you wish to maintain an official handicap. If you are a current lifetime member and do not wish to maintain a handicap, no action is required</p>';
+echo '<p>If you are currently a lifetime member please submit a check for $30.00 to cover the cost of maintaining your SCGA handicap. If you will be eligible for a lifetime membership (80 yrs.) before Jan. 1 of the coming year, submit your proof of age along with a check for $30.00 if you wish to maintain an official handicap. If you are a current lifetime member and do not wish to maintain a handicap, no action is required</p>';
 
 echo '    </div><!-- #content -->';
 echo ' </div><!-- #content-container -->' . PHP_EOL;
