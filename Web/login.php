8de086ea4d495bfd7b55fdcc14292ef51bff4273
DESKTOP-3GI47GH\albitz <palbitz@san.rr.com>
2018-12-04 18:10:10 -0800

    Add apple touch icons. Update documents. Update login.php for paulalbitz.com website after I cloned the coronadomensgolf.org website to paulalbitz.com.

--- Web/login.php
+++ Web/login.php
@@ -2,11 +2,11 @@
 // login.php
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 $web_site = 'paulalbitz.com';
-$db_hostname = 'mysql.' . $web_site;
-$db_database = 'cmgc_test'; 
+$db_hostname = 'cmgcdb.paulalbitz.com';
+$db_database = 'coronadomensgolf_test'; 
 $db_username = 'cmgc';
 $db_password = 'cor53Onado'; 
-$wp_folder = '/cmgc1';
+$wp_folder = '/wp';
 $script_folder = '/v2';
 $script_folder_href = 'v2/';
 $ipn_file = 'paypal_ipn.php';
