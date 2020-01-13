0ace0835b8c495f876433e574fedfb2e233a369c
DESKTOP-3GI47GH\albitz <palbitz@san.rr.com>
2020-01-13 08:36:44 -0800

    Update course handicap calculation to be relative to par instead of the course rating.

--- Web/submit_local_handicap.php
+++ Web/submit_local_handicap.php
@@ -55,7 +55,8 @@ if (! isset ( $_POST ['LocalHandicap'] )) {
 		else {
 			$lowerHandicap = (float)$LocalHandicapNeg;
 		}
-		$calculatedHandicap = round($lowerHandicap * 117.0 / 113.0);
+		// Slope is 117.0, Course rating is 70.2. Par is 72
+		$calculatedHandicap = round(($lowerHandicap * 117.0 / 113.0) + (70.2 - 72)) ;
 		$calculatedHandicap = str_replace("-", "+", (string)$calculatedHandicap);
 		
 		if (! $insert->bind_param ( 'isss', $_POST ['LocalHandicap'] [$i] ['GHIN'], $_POST ['LocalHandicap'] [$i] ['SCGAHandicap'], $_POST ['LocalHandicap'] [$i] ['LocalHandicap'], $calculatedHandicap )) {
