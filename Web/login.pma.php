baf9faa65ddee1be036271e12c5dd8ee197f6d18
DESKTOP-3GI47GH\albitz <palbitz@san.rr.com>
2021-07-15 08:51:57 -0700

    Update the test site database name

--- Web/login.pma.php
+++ Web/login.pma.php
@@ -3,7 +3,7 @@
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 $web_site = 'paulalbitz.com';
 $db_hostname = 'cmgcdb.paulalbitz.com';
-$db_database = 'coronadomensgolf_2021_july'; 
+$db_database = 'coronadomensgolf_2021_15'; 
 $db_username = 'cmgc';
 $db_password = 'pau58Onado'; 
 $wp_folder = '/wp';
