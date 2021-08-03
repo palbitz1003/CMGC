4ce083c0e43fca111b6a75404484444c8e32a865
DESKTOP-3GI47GH\albitz <palbitz@san.rr.com>
2021-08-03 14:10:55 -0700

    Add GHIN window

---
+++ WebAdmin/WebAdmin/View/AddGhinWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WebAdmin.View
+{
+    /// <summary>
+    /// Interaction logic for AddGhinWindow.xaml
+    /// </summary>
+    public partial class AddGhinWindow : Window
+    {
+        public string Ghin { get; set; }
+
+        public AddGhinWindow()
+        {
+            InitializeComponent();
+            GhinTextBox.Focus();
+        }
+
+        private void DoneButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(GhinTextBox.Text))
+            {
+                MessageBox.Show("Please fill in the GHIN number. 0 is a valid GHIN number.");
+                return;
+            }
+
+            int x;
+            if (!int.TryParse(GhinTextBox.Text, out x))
+            {
+                MessageBox.Show("Please enter a valid number");
+                return;
+            }
+
+            Ghin = GhinTextBox.Text;
+
+            DialogResult = true;
+            this.Close();
+        }
+    }
+}
