aa3e985d478ff1f4c16b671db9b0a0cec84630f1
DESKTOP-3GI47GH\albitz <palbitz@san.rr.com>
2022-11-05 13:16:23 -0700

    File from WebAdmin that should have been added to source control already ...

---
+++ WebAdmin/WebAdmin/Model/ReplacePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WebAdmin
+{
+    public class ReplacePlayer : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public Player Remove;
+        public Player Add;
+    }
+}
