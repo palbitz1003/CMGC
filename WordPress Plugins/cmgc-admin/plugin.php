f2f8db7d1384f988cbed310557433f40cba3b814
DESKTOP-3GI47GH\albitz <palbitz@san.rr.com>
2022-12-11 16:09:14 -0800

    Add to plugin to display membership applications

--- WordPress Plugins/cmgc-admin/plugin.php
+++ WordPress Plugins/cmgc-admin/plugin.php
@@ -15,14 +15,22 @@
         'edit_pages', 'cmgc-admin-menu', 'cmgc_admin_show_menu',
         'dashicons-admin-home', 99);
     
-    add_submenu_page('cmgc-admin-menu', 'Membership Waitlist', 'Membership Waitlist',
-        'edit_pages', 'cmgc-admin-membership-waitlist', 'cmgc_admin_membership_waitlist_page');
+    add_submenu_page('cmgc-admin-menu',  // slug name for parent menu
+        'Membership Waitlist', // title of page
+        'Membership Waitlist', // name of sub-menu
+        'edit_pages', // minimum capability (editor)
+        'cmgc-admin-membership-waitlist', // slub name for submenu
+        'cmgc_admin_membership_waitlist_page'); // function to call
+
+    add_submenu_page('cmgc-admin-menu', 'Membership Applications', 'Membership Applications',
+        'edit_pages', 'cmgc-admin-membership-applications', 'cmgc_admin_membership_application_page');
 
  }
 
  function cmgc_admin_plugin_create_options()
  {
     // Only load if explicitly needed
+    // Use table to pass back result of upload to waitlist page
     add_option('cmgc_admin_plugin_options', array(
         'waiting_list_upload_results' => ''
         ), 
@@ -80,6 +88,7 @@
     <?php
  }
 
+ // When the user clicks on the "submit" waiting list button, this function is called
  add_action( 'admin_action_cmgc_admin_upload_waitlist', 'cmgc_admin_upload_waitlist_action' );
 function cmgc_admin_upload_waitlist_action()
 {
@@ -102,6 +111,7 @@ function cmgc_admin_upload_waitlist_action()
     }
 }
 
+// At the bottom of the waiting list page, the people approved to become members (payment due is non-zero) is shown
 function cmgc_admin_show_waitlist_with_payment_due(){
     // Putting require_once at the top of this file didn't work
     require_once realpath($_SERVER["DOCUMENT_ROOT"]) . '/login.php';
@@ -184,6 +194,126 @@ function cmgc_admin_show_waitlist_with_payment_due(){
     echo '</tbody>' . PHP_EOL;
     echo '</table>' . PHP_EOL;
  }
+
+ function cmgc_admin_membership_application_page()
+ {
+    // Putting require_once at the top of this file didn't work
+    require_once realpath($_SERVER["DOCUMENT_ROOT"]) . '/login.php';
+
+    $connection = new mysqli ('p:' . $db_hostname, $db_username, $db_password, $db_database );
+    
+    if ($connection->connect_error){
+        echo 'Database connection error: ' .  $connection->connect_error . "<br>";
+        return;
+    }
+    
+    $sqlCmd = "SELECT * FROM `MembershipApplication` WHERE `Active` = 1 ORDER BY `DateTimeAdded` ASC";
+    $query = $connection->prepare ( $sqlCmd );
+    
+    if (! $query) {
+        die ( $sqlCmd . " prepare failed: " . $connection->error );
+    }
+    
+    if (! $query->execute ()) {
+        die ( $sqlCmd . " execute failed: " . $connection->error );
+    }
+
+    class MembershipApplication {
+        public $RecordKey;
+        public $Active;
+        public $LastName;
+        public $FirstName;
+        public $MailingAddress;
+        public $Email;
+        public $GHIN;
+        public $PhoneNumber;
+        public $BirthDate;
+        public $Sponsor1LastName;
+        public $Sponsor1Ghin;
+        public $Sponsor1PhoneNumber;
+        public $Sponsor2LastName;
+        public $Sponsor2Ghin;
+        public $Sponsor2PhoneNumber;
+        public $DateTimeAdded;
+        public $Payment;
+        public $PaymentDateTime;
+        public $PayerName;
+    }
+    
+    $query->bind_result ( $recordKey, $active, $lastName, $firstName, $mailingAddress, $email, $ghin, $phoneNumber, $birthDate,
+                        $sponsor1LastName, $sponsor1Ghin, $sponsor1PhoneNumber,
+                        $sponsor2LastName, $sponsor2Ghin, $sponsor2PhoneNumber,
+                        $dateTimeAdded, $payment, $paymentDateTime, $payerName );
+    
+    $membershipApplicationEntries = array();
+    while ( $query->fetch () ) {
+        $membershipApplication = new MembershipApplication();
+        $membershipApplication->RecordKey = $recordKey;
+        $membershipApplication->Active = $active;
+        $membershipApplication->LastName = $lastName;
+        $membershipApplication->FirstName = $firstName;
+        $membershipApplication->MailingAddress = $mailingAddress;
+        $membershipApplication->Email = $email;
+        $membershipApplication->GHIN = $ghin;
+        $membershipApplication->PhoneNumber = $phoneNumber;
+        $membershipApplication->BirthDate = $birthDate;
+        $membershipApplication->Sponsor1LastName = $sponsor1LastName;
+        $membershipApplication->Sponsor1Ghin = $sponsor1Ghin;
+        $membershipApplication->Sponsor1PhoneNumber = $sponsor1PhoneNumber;
+        $membershipApplication->Sponsor2LastName = $sponsor2LastName;
+        $membershipApplication->Sponsor2Ghin = $sponsor2Ghin;
+        $membershipApplication->Sponsor2PhoneNumber = $sponsor2PhoneNumber;
+        $membershipApplication->DateTimeAdded = $dateTimeAdded;
+        $membershipApplication->Payment = $payment;
+        $membershipApplication->PaymentDateTime = $paymentDateTime;
+        $membershipApplication->PayerName = $payerName;
+
+        $membershipApplicationEntries[] = $membershipApplication;
+    }
+    
+    $query->close ();
+
+    echo '<h2>Pending Membership Applications</h2>' ;
+    if (! $membershipApplicationEntries || (count ( $membershipApplicationEntries ) == 0)) {
+        echo "none pending";
+        return;
+    }
+
+    // Table class can be widefat, fixed, or striped
+    echo '<table class="fixed" >' . PHP_EOL;
+    echo '<thead><tr><th>Date Added</th><th>Last</th><th>First</th><th>GHIN</th><th>Mailing Address</th><th>Email Address</th><th>Phone</th><th>DOB</th>';
+    echo '<th>Sp1 Last</th><th>Sp1 GHIN</th><th>Sp1 Phone</th><th>Sp2 Last</th><th>Sp2 GHIN</th><th>Sp2 Phone</th><th>Payment</th><th>Payment Date</th><th>Payer Name</th></tr></thead>' . PHP_EOL;
+    echo '<tbody>' . PHP_EOL;
+
+    for($i = 0; $i < count ( $membershipApplicationEntries ); ++ $i) {
+
+        echo '<tr>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->DateTimeAdded . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->LastName . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->FirstName . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->GHIN . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->MailingAddress . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->Email . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->PhoneNumber . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->BirthDate . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->Sponsor1LastName . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->Sponsor1Ghin . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->Sponsor1PhoneNumber . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->Sponsor2LastName . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->Sponsor2Ghin . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->Sponsor2PhoneNumber . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->Payment . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->PaymentDateTime . '</td>' . PHP_EOL;
+        echo '<td style="padding: 0px 10px 0px 10px;">' . $membershipApplicationEntries[$i]->PayerName . '</td>' . PHP_EOL;
+        echo '</tr>' . PHP_EOL;
+    }
+
+
+    // Finish the first column table.  Show the 2nd column table.
+    echo '</tbody>' . PHP_EOL;
+    echo '</table>' . PHP_EOL;
+ }
+ 
 
  /*
  function add_custom_menu_item(){
