e86f781b781eefee8142e027f64e7da0bc3dce7f
DESKTOP-3GI47GH\albitz <palbitz@san.rr.com>
2022-12-28 10:47:04 -0800

    Update database for palbitz site

--- Web/login.pma.php
+++ Web/login.pma.php
@@ -3,7 +3,7 @@
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 $web_site = 'paulalbitz.com';
 $db_hostname = 'cmgcdb.paulalbitz.com';
-$db_database = 'cmgc_2021_12_22'; 
+$db_database = 'cmgc_2022_dec'; 
 $db_username = 'cmgc';
 $db_password = 'pau58Onado'; 
 $wp_folder = '/wp';
